ea1ec81f7dddcc6276ada18631e756f9bbebe381
James Lewis <james.lewis@jhlws.com>
2019-11-28 13:04:18 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "prefix" {
   description = "The prefix used for all resources"
-  default = "fairdev"
 }
 
 variable "location" {
   description = "The Azure location where all resources should be created"
-  default = "uksouth"
 }
 
 variable "subscription" {
