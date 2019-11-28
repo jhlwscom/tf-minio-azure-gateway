e4e0b810e58978bd90ce0b77829eec28706e21f0
James Lewis <james.lewis@jhlws.com>
2019-11-28 14:32:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ resource "azurerm_storage_account" "main" {
   name                = "${var.prefix}sa"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
+  account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
